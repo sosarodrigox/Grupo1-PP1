673b0248ffc041c00ef8d69ea273c06b46be2649
sosarodrigox <sosarodrigox@gmail.com>
2023-10-24 21:01:26 -0300

    cambiado titulo y markdown

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Body Fat Prediction App")
+
+st.markdown('<h1 style="color:yellow;">Body Performance</h1>', unsafe_allow_html=True)
+
+st.subheader(
+    "Classification App V1.0",
+)
+st.markdown(
+    "This app is used to help to classify body performance based on the data inputted by the user."
+)
