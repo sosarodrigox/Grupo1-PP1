46ddaa90be53f39d846da5beda4f6d81af5c5fb8
sosarodrigox <sosarodrigox@gmail.com>
2023-10-24 22:53:07 -0300

    update columns

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ html_title = """
 st.markdown(html_title, unsafe_allow_html=True)
 
 html_subtitle = """
-<p style="color:#9ea0a3;text-align:center;">This app is used to help to classify body performance based on the data inputted by the user</p>
+<p style="color:#9ea0a3;text-align:center;">Esta aplicación ha sido diseñada para ayudar a clasificar el rendimiento corporal de las personas en función de los datos ingresados por el usuario.</p>
     </div>"""
 
 st.markdown(html_subtitle, unsafe_allow_html=True)
@@ -17,14 +17,18 @@ st.markdown(html_subtitle, unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header("Ingrese los datos")
-
-    # Lecctura de datos
-    # Datos = st.text_input("Ingrese los valores : N P K Temp Hum pH lluvia:")
-    N = st.text_input("Nitrógeno:")
-    P = st.text_input("Fósforo:")
-    K = st.text_input("Potasio:")
-    Temp = st.text_input("Temperatura:")
-    Hum = st.text_input("Humedad:")
-    pH = st.text_input("pH:")
-    rain = st.text_input("Lluvia:")
+    st.subheader("Ingrese datos:")
+    edad = st.text_input("Edad:")
+    genero = st.text_input("Género:")
+    alto_m = st.text_input("Altura (m):")
+    peso_kg = st.text_input("Peso (kg):")
+    grasa_corporal = st.text_input("Grasa corporal (%):")
+    IMC = st.text_input("IMC:")
+with col2:
+    st.subheader(" ")
+    diastolica = st.text_input("Presión diastólica:")
+    sistolica = st.text_input("Presión sistólica:")
+    fuerza_agarre = st.text_input("Fuerza de agarre:")
+    sentarse_inclinarse_m = st.text_input("Sentarse e inclinarse (m):")
+    cant_abdominales = st.text_input("Cantidad de abdominales:")
+    salto_largo_m = st.text_input("Salto largo (m):")
