da7136bc8f90c7f61c63168f124b1a4f05b2de99
sosarodrigox <sosarodrigox@gmail.com>
2023-10-24 22:14:44 -0300

    actualizado readme

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
 - Mauricio Zenere (zeneremauricio@gmail.com) - Sala 2
 - Carolina Margheim (caro08.m@gmail.com) - Sala 2
 - Ma. Eugenia Ingaramo (eugeinga15@gmail.com) - Sala 1
-- Rodrigo Sosa (sosarodrigox@gmail.com) - Sala 1
+- Rodrigo Sosa (sosarodrigox@gmail.com) - Sala 2
 - Manuel Ontiveros (manu.ontiveros023@gmail.com) - Sala 2
 
 ### Recursos:
 - Trello: https://trello.com/w/ispcgrupo1
-- Streamlit: Para correr la app ejecutar el comando "streamlit run body_fat_prediction.py" (Desde carpeta raiz)
+- Streamlit: Para correr la app ejecutar el comando "streamlit run app.py" (Desde carpeta raiz)
--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import streamlit as st
 import pandas as pd
 
+# Título
+html_title = """
+    <h2 style="color:#ebd234;text-align:center;">Body Performance Classification App V1.0 </h2>
+    </div>
+    """
+st.markdown(html_title, unsafe_allow_html=True)
 
-st.markdown('<h1 style="color:yellow;">Body Performance</h1>', unsafe_allow_html=True)
+html_subtitle = """
+<p style="color:#9ea0a3;text-align:center;">This app is used to help to classify body performance based on the data inputted by the user</p>
+    </div>"""
 
-st.subheader(
-    "Classification App V1.0",
-)
-st.markdown(
-    "This app is used to help to classify body performance based on the data inputted by the user."
-)
+st.markdown(html_subtitle, unsafe_allow_html=True)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("Ingrese los datos")
+
+    # Lecctura de datos
+    # Datos = st.text_input("Ingrese los valores : N P K Temp Hum pH lluvia:")
+    N = st.text_input("Nitrógeno:")
+    P = st.text_input("Fósforo:")
+    K = st.text_input("Potasio:")
+    Temp = st.text_input("Temperatura:")
+    Hum = st.text_input("Humedad:")
+    pH = st.text_input("pH:")
+    rain = st.text_input("Lluvia:")
